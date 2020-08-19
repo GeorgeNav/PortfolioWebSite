f834eb68ff2927b024eb315f3e65d8e4ddf65909
GeorgeNav <george.g.navarro@gmail.com>
2020-08-19 14:40:37 -0500

    allowed build folder to be pushed

---
+++ build/precache-manifest.c1889a46d7e4a02eaee40f12249bf0f5.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac47c49f6ab646657668aac8c2858bc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6e8176b3feba38de6530",
+    "url": "./static/css/main.43152b5c.chunk.css"
+  },
+  {
+    "revision": "082803e01878ac3ae730",
+    "url": "./static/js/2.47ff937e.chunk.js"
+  },
+  {
+    "revision": "68f1ae2565bff3d8d66dc7914bdc8c55",
+    "url": "./static/js/2.47ff937e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e8176b3feba38de6530",
+    "url": "./static/js/main.51f9b6e9.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  },
+  {
+    "revision": "9435a4a41a16c1e57709ab591fb04b01",
+    "url": "./static/media/answered_question.9435a4a4.png"
+  },
+  {
+    "revision": "4715f047cc18690e97e4ece2f451e3e6",
+    "url": "./static/media/create_profile.4715f047.png"
+  },
+  {
+    "revision": "0a0434688a1bd1ea04d9b53143793716",
+    "url": "./static/media/dart.0a043468.svg"
+  },
+  {
+    "revision": "67794f294bbfa23ffa347019f15ed39a",
+    "url": "./static/media/doc_file_icon.67794f29.svg"
+  },
+  {
+    "revision": "6ee7dbeed691403c1528e7e212c20827",
+    "url": "./static/media/earth.6ee7dbee.svg"
+  },
+  {
+    "revision": "a5149d4623be001efc9e0f668a51cdb9",
+    "url": "./static/media/fire.a5149d46.gif"
+  },
+  {
+    "revision": "c521e7e67222276a6860665a46813cc6",
+    "url": "./static/media/firebase.c521e7e6.png"
+  },
+  {
+    "revision": "0fcac2331cec60aff8d010bfbad945c5",
+    "url": "./static/media/fuel_consolidator.0fcac233.png"
+  },
+  {
+    "revision": "0c0e0e90224628b66bc5608af7093305",
+    "url": "./static/media/history.0c0e0e90.png"
+  },
+  {
+    "revision": "2a416fe6df51d3519e8bd37702328788",
+    "url": "./static/media/home.2a416fe6.png"
+  },
+  {
+    "revision": "891a71b9a98f1d58491e1b06c4d356a2",
+    "url": "./static/media/javascript.891a71b9.svg"
+  },
+  {
+    "revision": "c3cd3be74f490734c47f43c637677faa",
+    "url": "./static/media/lake.c3cd3be7.jpg"
+  },
+  {
+    "revision": "685c201210aba48d12d085471fa1b2c1",
+    "url": "./static/media/leaderboard.685c2012.png"
+  },
+  {
+    "revision": "8c6f15d9aa4c7b18963f8ecbd928d5d6",
+    "url": "./static/media/login.8c6f15d9.png"
+  },
+  {
+    "revision": "54d7bddcd3019ec3c3b23db00160ccef",
+    "url": "./static/media/microsoft_transact_sql.54d7bddc.svg"
+  },
+  {
+    "revision": "aeb3c06c326e5a03e85f571f87985455",
+    "url": "./static/media/moon.aeb3c06c.png"
+  },
+  {
+    "revision": "f80a91ba03fbf0e99ea6949aa3a6399e",
+    "url": "./static/media/my_sql.f80a91ba.svg"
+  },
+  {
+    "revision": "c8a20c968c749a11059350554fbcf341",
+    "url": "./static/media/new_question.c8a20c96.png"
+  },
+  {
+    "revision": "90a7a88d73315e97644e9e0f00c67461",
+    "url": "./static/media/nodejs.90a7a88d.png"
+  },
+  {
+    "revision": "5a9681c876cc158bf5e71eaa13bc162e",
+    "url": "./static/media/pdf_file_icon.5a9681c8.svg"
+  },
+  {
+    "revision": "1b7efe195db472aa87fc60441d9e9d35",
+    "url": "./static/media/profile.1b7efe19.png"
+  },
+  {
+    "revision": "abbcbb5172ce6ade39071f880409ac71",
+    "url": "./static/media/quote.abbcbb51.png"
+  },
+  {
+    "revision": "29c9e5c96c96944ce271288d0461341b",
+    "url": "./static/media/react_bootstrap.29c9e5c9.png"
+  },
+  {
+    "revision": "e6d366ac31ded5f21e9ccdee93edc620",
+    "url": "./static/media/react_redux.e6d366ac.png"
+  },
+  {
+    "revision": "186c2e9bb1294ab0faaf0de31d774cde",
+    "url": "./static/media/spacecraft.186c2e9b.svg"
+  },
+  {
+    "revision": "37d724678e1c58d21864b5d767851f30",
+    "url": "./static/media/would_you_rather.37d72467.png"
+  }
+]);
