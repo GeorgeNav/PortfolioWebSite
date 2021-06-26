b3611cb89083eb8df6f22a46d309664307c3ea4d
GeorgeNav <george.g.navarro@gmail.com>
2021-06-26 00:37:04 -0500

    router fix?

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from 'components/App'
 import { ThemeProvider, createMuiTheme } from '@material-ui/core'
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const theme = createMuiTheme({
   typography: {
