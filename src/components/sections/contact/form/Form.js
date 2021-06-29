6b9496730dfedcb0e2fd480a445b6bcfb1bdf386
GeorgeNav <george.g.navarro@gmail.com>
2021-06-28 21:37:23 -0500

    added email logic

--- src/components/sections/contact/form/Form.js
+++ src/components/sections/contact/form/Form.js
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Grid, TextField, Button, Typography } from '@material-ui/core'
 import { useForm } from 'react-hook-form'
+import { sendForm } from 'emailjs-com'
 
 const formVariant = 'filled'
 
 const Form = () => {
-  const { register, handleSubmit } = useForm()
+  const formRef = useRef()
+  const { register, /* handleSubmit, errors */ } = useForm()
 
-  const onSubmit = (data) => {
-    console.log(data)
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    sendForm(
+      'service_znjosyj',
+      'template_v1dlasy',
+      e.target,
+      'user_HlNFnApaJz30khi5FiSOE',
+    )
   }
 
-  const onError = (data) => {
+  /* const onError = (data) => {
     console.log(data)
-  }
+  } */
 
   return <form
-    onSubmit={handleSubmit(onSubmit, onError)}>
+    ref={formRef}
+    //onSubmit={handleSubmit(onSubmit, onError)}
+    onSubmit={onSubmit}>
     <Grid container
       spacing={2}>
       <Grid item container
