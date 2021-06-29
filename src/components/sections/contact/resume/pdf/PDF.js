bdb5e4b58ba847da6a534ae96acc1b7b9454c278
GeorgeNav <george.g.navarro@gmail.com>
2021-06-29 15:51:49 -0500

    pdf embedded for mobile

--- src/components/sections/contact/resume/pdf/PDF.js
+++ src/components/sections/contact/resume/pdf/PDF.js
@@ -10,7 +10,9 @@ const PDF = ({ pdfUrl }) => {
     data={pdfUrl + '#toolbar=0&navpanes=0&scrollbar=0'}
     width='100%'
     height='100%'
-    type='application/pdf'/>
+    type='application/pdf'>
+    <embed src={pdfUrl} height='100%' style={{minHeight: 400}}/>
+  </object>
 }
 
 PDF.propTypes = {
